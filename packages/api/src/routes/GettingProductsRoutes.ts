c5b40de81c154357cec4519ff99ce0721d3c73d5
Buriches <01121998kiril@gmail.com>
2023-06-06 02:53:06 +0300

    fix: path of queries

--- packages/api/src/routes/GettingProductsRoutes.ts
+++ packages/api/src/routes/GettingProductsRoutes.ts
@@ -3,13 +3,16 @@ const Router = require('express')
 const router = new Router()
 const gettingProductsController = require('../controllers/GettingProductsController')
 
-router.get('/get24/:from', gettingProductsController.get24)
-router.get('/get24withCategory/', gettingProductsController.get24withCategory)
+router.get('/get24', gettingProductsController.get24)
+router.get('/get24withCategory', gettingProductsController.get24withCategory)
+router.get('/get1ByPath', gettingProductsController.get1ByPath)
 router.get('/getProductsByName/', gettingProductsController.getProductsByName)
 router.get('/getMainCategories/', gettingProductsController.getMainCategories)
-router.get('/getUnderCategories/', gettingProductsController.getUnderCategories)
+router.get('/getUnderCategories', gettingProductsController.getUnderCategories)
+router.get('/getAllCategories', gettingProductsController.getAllCategories)
 router.get('/getProductPrices/:good_id', gettingProductsController.getProductPrices)
 router.get('/getSameProducts', gettingProductsController.getSameProducts)
+router.get('/getMarkets', gettingProductsController.getMarkets)
 
 
 module.exports = router
