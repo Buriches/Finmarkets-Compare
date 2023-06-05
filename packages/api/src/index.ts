8967f95f8398aada3fa151d717dd0a1e87d09a70
Buriches <01121998kiril@gmail.com>
2023-06-06 02:43:28 +0300

    fix: required headers

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -4,7 +4,12 @@ const gettingProductsRouter = require('./routes/GettingProductsRoutes')
 const PORT = process.env.PORT || 1500
 
 const app = express()
-
+app.use(function(req: any, res: any, next: any) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, application/json');
+  next();
+});
 app.use(express.json())
 app.use('/get', gettingProductsRouter)
 
